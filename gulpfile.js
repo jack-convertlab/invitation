facb90ca41155b772ab6111e97fa975acc97a5d5
jack.zhu <jack.zhu@convertlab.com>
2016-05-12 15:14:09 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,37 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 
-var destPath='./build';
+var destPath='build';
 
 gulp.task('css',function() {
-  return gulp.src(['css/normalize.css','css/custom.css'])
+  return gulp.src(['public/css/normalize.css','public/css/custom.css'])
     .pipe(concat('app.min.css'))
     .pipe(minifyCss())
     .pipe(gulp.dest(destPath+'/css'));
 });
 gulp.task('js',function () {
-  return gulp.scr(['js/TweenLite.min.js','js/CSSPlugin.min.js','js/main.js'])
+  return gulp.src([
+    'public/js/TweenLite.min.js',
+    'public/js/CSSPlugin.min.js',
+    'bublic/js/main.js'])
     .pipe(concat('app.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest(destPath+'/js'));
 });
 gulp.task('html',()=>{
-  return gulp.src('./index.html')
+  return gulp.src('public/index.html')
     .pipe(htmlReplace({
       'css-app':'css/app.min.css',
       'js-app':'js/app.min.js'
     }))
     .pipe(gulp.dest(destPath));
 })
+gulp.task('copy',()=>{
+  return gulp.src([
+    'public/img/*'])
+    .pipe(gulp.dest(destPath+'/img'));
+});
 
-gulp.task('default',['css','js','html'], ()=>{
+gulp.task('default',['copy','css','js','html'], ()=>{
   
 })
